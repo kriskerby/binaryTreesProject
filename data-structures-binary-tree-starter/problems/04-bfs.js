30fb7b286dd5ac6727156b9e391b64500f144638
Gerryl Esperacion <Gerrylesperacion@gmail.com>
2021-04-27 09:28:43 -0700

    complete bfs

--- data-structures-binary-tree-starter/problems/04-bfs.js
+++ data-structures-binary-tree-starter/problems/04-bfs.js
@@ -4,6 +4,21 @@
 // in the order you visited them.
 function bfs(root){
     // Your code here
+    if (!root) return []
+    let queue = [root]
+    let nodes = []
+
+    while (queue.length){
+        let node = queue.shift()
+        nodes.push(node.val)
+        if (node.left) {
+            queue.push(node.left)
+        }
+        if (node.right) {
+            queue.push(node.right)
+        }
+    }
+    return nodes
 }
 
 module.exports = { bfs };
