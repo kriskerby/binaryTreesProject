a5d31a6e7d72e1a6564c2efa26480bae778f4a2c
kriskerby <kriskerby@gmail.com>
2021-04-27 09:11:48 -0700

    dfs done

--- data-structures-binary-tree-starter/problems/03-dfs.js
+++ data-structures-binary-tree-starter/problems/03-dfs.js
@@ -3,7 +3,40 @@
 // order, and returns an array containing the values
 // in the order you visited them.
 function dfs(root){
-    // Your code here
+    if (!root) return [];
+
+
+
+
+    let stack = [root];
+    let nodes = [];
+
+
+
+    while(stack.length){
+        let node = stack.pop();
+        nodes.push(node.val);
+        if (node.right){
+            stack.push(node.right);
+        }
+        if (node.left){
+            stack.push(node.left);
+        }
+    }
+return nodes;
 }
+
+
+// function dfs(root){
+//     if (!root) return [];
+//     let newArr = [];
+//     newArr.push(root.val)
+//     newArr.push(...dfs(root.left))
+//     newArr.push(...dfs(root.right))
+
+//     return newArr;
+// }
+
+
 
 module.exports = { dfs };
